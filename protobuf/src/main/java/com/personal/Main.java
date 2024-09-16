00ca2f30d3677fc3c48a7b3c73968333c3593a05
jacco <jacco@foxmail.com>
2024-09-16 17:28:22 +0800

    增加protobuf配置

--- protobuf/src/main/java/com/personal/Main.java
+++ protobuf/src/main/java/com/personal/Main.java
@@ -1,10 +1,15 @@
 package com.personal;
 
+import com.study.protobuf.entity.PeopleInfo;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        PeopleInfo peopleInfo = PeopleInfo.newBuilder().setAge(10).setName("Jacco").build();
+        System.out.println(peopleInfo.toByteArray().length);
+
     }
 }
